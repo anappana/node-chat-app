52af6e0d13238bebf9d8bcd10919496cdcec13d7
Xianchu <xxc141730@gmail.com>
2017-11-19 14:58:08 -0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Mike',
-    text: 'Hey. What are you doing?'
-  });
-
 });
 
 socket.on('disconnect', function () {
